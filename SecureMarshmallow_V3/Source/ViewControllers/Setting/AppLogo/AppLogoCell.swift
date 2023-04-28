ae94198cd0e854602a4d372be912950278757d4f
junha <goodjunha@gmail.com>
2023-04-29 01:43:54 +0900

    🔨:: 로고들 바꾸는 거 추가

--- SecureMarshmallow_V3/Source/ViewControllers/Setting/AppLogo/AppLogoCell.swift
+++ SecureMarshmallow_V3/Source/ViewControllers/Setting/AppLogo/AppLogoCell.swift
@@ -5,4 +5,33 @@
 //  Created by 박준하 on 2023/04/28.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class AppLogoCell: UICollectionViewCell {
+    
+    static var identifier = "AppLogoCell"
+    
+    var appLogoView = UIImageView().then {
+        $0.backgroundColor = .red
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func layout() {
+        contentView.addSubview(appLogoView)
+        
+        appLogoView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
--- SecureMarshmallow_V3/Source/ViewControllers/Setting/AppLogo/AppLogoViewController.swift
+++ SecureMarshmallow_V3/Source/ViewControllers/Setting/AppLogo/AppLogoViewController.swift
@@ -5,4 +5,62 @@
 //  Created by 박준하 on 2023/04/28.
 //
 
-import Foundation
+import UIKit
+import Then
+import SnapKit
+
+class AppLogoViewController: UIViewController {
+    
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.register(AppLogoCell.self, forCellWithReuseIdentifier: AppLogoCell.identifier)
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.bottom.equalToSuperview().offset(-20)
+
+        }
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.reloadData()
+    }
+}
+
+extension AppLogoViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AppLogoCell.identifier, for: indexPath) as! AppLogoCell
+        
+        cell.backgroundColor = .blue
+        cell.layer.cornerRadius = 15
+        cell.appLogoView.image = UIImage(named: "BlackAppIcon")
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("1")
+    }
+}
+
+extension AppLogoViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let spacing: CGFloat = 10
+        let totalSpacing: CGFloat = (2 * spacing) + (2 * spacing)
+        let cellWidth = (collectionView.bounds.width - totalSpacing) / 3
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+}
