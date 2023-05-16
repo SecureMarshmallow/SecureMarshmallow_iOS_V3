3e4996ed2143aef2b8559c1af5df73ff60f9e31a
junha <goodjunha@gmail.com>
2023-05-16 16:18:18 +0900

    🔨:: layout homeViewContrller 추가

--- SecureMarshmallow_V3/Source/ViewControllers/HomeViewController.swift
+++ SecureMarshmallow_V3/Source/ViewControllers/HomeViewController.swift
@@ -1,24 +1,239 @@
 import UIKit
 import Then
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    private lazy var navLabel = UILabel().then {
-        $0.textColor = UIColor.black
-        $0.text = "SecureMarshmallow"
-        $0.font = .systemFont(ofSize: 24.0, weight: .bold)
-    }
+    private lazy var navLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        label.text = "SecureMarshmallow"
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        return label
+    }()
     
+    var collectionView: UICollectionView!
+    var items: [[Int]] = [
+        [3, 1, 1, 1, 1, 2, 4, 4]
+    ]
+    
+    let cellIdentifier = "cell"
+    var movingCellSnapshot: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 24, left: 20, bottom: 24, right: 20)
+
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        collectionView.backgroundColor = .white
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongGesture(_:)))
+        collectionView.addGestureRecognizer(longPressGesture)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         configureUI()
-        view.backgroundColor = .white
     }
     
     func configureUI() {
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: navLabel)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: navLabel)
         self.navigationItem.leftItemsSupplementBackButton = true
+    }
+    
+    @objc func handleLongGesture(_ gesture: UILongPressGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            guard let selectedIndexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else {
+                break
+            }
+            collectionView.beginInteractiveMovementForItem(at: selectedIndexPath)
+
+            let cell = collectionView.cellForItem(at: selectedIndexPath)!
+            
+            movingCellSnapshot = cell.snapshotView(afterScreenUpdates: false)
+            movingCellSnapshot?.center = cell.center
+            collectionView.addSubview(movingCellSnapshot!)
+            cell.isHidden = true
+        case .changed:
+            collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: collectionView))
+            movingCellSnapshot?.center = gesture.location(in: collectionView)
+        case .ended:
+            collectionView.endInteractiveMovement()
+            if let snapshot = movingCellSnapshot {
+                snapshot.removeFromSuperview()
+            }
+            if let selectedIndexPath = collectionView.indexPathsForSelectedItems?.first {
+                collectionView.cellForItem(at: selectedIndexPath)?.isHidden = false
+            }
+        default:
+            collectionView.cancelInteractiveMovement()
+            if let snapshot = movingCellSnapshot {
+                snapshot.removeFromSuperview()
+            }
+            if let selectedIndexPath = collectionView.indexPathsForSelectedItems?.first {
+                collectionView.cellForItem(at: selectedIndexPath)?.isHidden = false
+            }
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        cell.backgroundColor = .gray
+        
+        for subview in cell.contentView.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        let item = items[indexPath.section][indexPath.item]
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 20)
+        label.textAlignment = .center
+        label.textColor = .white
+        label.text = "Item \(item)"
+        cell.contentView.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard indexPath.section < items.count, indexPath.item < items[indexPath.section].count else {
+            return .zero
+        }
+        
+        let item = items[indexPath.section][indexPath.item]
+        var cellSize = CGSize.zero
+        
+        switch item {
+        case 1:
+            cellSize = CGSize(width: 80, height: 80)
+        case 2:
+            if traitCollection.horizontalSizeClass == .regular && traitCollection.verticalSizeClass == .regular {
+                cellSize = CGSize(width: 370, height: 230)
+            } else {
+                cellSize = CGSize(width: 390, height: 230)
+            }
+        case 3:
+            if traitCollection.horizontalSizeClass == .regular && traitCollection.verticalSizeClass == .regular {
+                cellSize = CGSize(width: 370, height: 260)
+            } else {
+                cellSize = CGSize(width: 390, height: 290)
+            }
+        case 4:
+            cellSize = CGSize(width: 150, height: 150)
+        case 5:
+            if traitCollection.horizontalSizeClass == .regular && traitCollection.verticalSizeClass == .regular {
+                cellSize = CGSize(width: 370, height: 370)
+            } else {
+                cellSize = CGSize(width: 390, height: 390)
+            }
+        default:
+            break
+        }
+        
+        return cellSize
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        guard indexPath.section < items.count, indexPath.item < items[indexPath.section].count else {
+//            return .zero
+//        }
+//
+//        let item = items[indexPath.section][indexPath.item]
+//        var cellSize = CGSize.zero
+//
+//        switch item {
+//        case 1:
+//            cellSize = CGSize(width: 80 - 20, height: 80 - 20)
+//        case 2:
+//            cellSize = CGSize(width: 370 - 20, height: 230 - 20)
+//        case 3:
+//            cellSize = CGSize(width: 370 - 20, height: 260 - 20)
+//        case 4:
+//            cellSize = CGSize(width: 150 - 20, height: 150 - 20)
+//        case 5:
+//            cellSize = CGSize(width: 370 - 20, height: 370 - 20)
+//        default:
+//            break
+//        }
+//
+//        return cellSize
+//    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 24, left: 20, bottom: 24, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        return CGSize(width: screenWidth, height: 60)
+    }
+    
+}
+
+extension HomeViewController {
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = items[indexPath.section][indexPath.item]
+        print("Selected item: \(item)")
+        print(items)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+
+        return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = items[sourceIndexPath.section].remove(at: sourceIndexPath.item)
+        items[destinationIndexPath.section].insert(item, at: destinationIndexPath.item)
+        
+        if sourceIndexPath.section == destinationIndexPath.section {
+
+            collectionView.reloadSections([sourceIndexPath.section])
+        } else {
+
+            collectionView.reloadSections([sourceIndexPath.section, destinationIndexPath.section])
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, targetIndexPathForMoveFromItemAt originalIndexPath: IndexPath, toProposedIndexPath proposedIndexPath: IndexPath) -> IndexPath {
+
+        let adjustedIndexPath = IndexPath(item: (collectionView.numberOfItems(inSection: proposedIndexPath.section) / 2), section: proposedIndexPath.section)
+        return adjustedIndexPath
     }
 }
