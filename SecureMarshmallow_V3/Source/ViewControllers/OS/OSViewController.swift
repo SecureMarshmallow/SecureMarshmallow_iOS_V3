eab4399bd24b19964df70aaa832893e54a041a85
junha <goodjunha@gmail.com>
2023-05-23 18:12:14 +0900

    🌊:: OS MVP 관련 코드

--- SecureMarshmallow_V3/Source/ViewControllers/OS/OSViewController.swift
+++ SecureMarshmallow_V3/Source/ViewControllers/OS/OSViewController.swift
@@ -3,71 +3,25 @@ import SnapKit
 
 class OSViewController: BaseEP {
     
+    private lazy var presenter = OSPresenter(viewController: self, navigationController: navigationController!)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateWith(self)
-        collectionView.dataSource = self
+        collectionView.dataSource = presenter
     }
     
     override func updateWith(_ controller: UIViewController) {
         super.updateWith(controller)
         
-        nameLabel.text = "iOS"
         customImageView.image = UIImage(named: "iPhone")
     }
+}
+
+extension OSViewController: OSProtocol {
     
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return 6
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let device = UIDevice.current
-        
-        let deviceName = device.name
-        let systemName = device.systemName
-        let systemVersion = device.systemVersion
-        let model = device.model
-        let localizedModel = device.localizedModel
-        let uuid = device.identifierForVendor?.uuidString ?? "Unknown"
-        
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExplanationCollectionViewCell.identifier, for: indexPath) as? ExplanationCollectionViewCell
-        
-        cell?.layer.cornerRadius = 20.0
-        cell?.backgroundColor = .cellColor
-        cell?.layer.shadowOffset = CGSize(width: 10, height: 10)
-        cell?.layer.shadowOpacity = 0.1
-        
-        if indexPath.row == 0 {
-            cell?.titleLabel.text = "디바이스 이름"
-            cell?.descriptionLabel.text = "\(deviceName)"
-        }
-        if indexPath.row == 1 {
-            cell?.titleLabel.text = "OS 이름"
-            cell?.descriptionLabel.text = "\(systemName)"
-        }
-        if indexPath.row == 2 {
-            cell?.titleLabel.text = "버전"
-            cell?.descriptionLabel.text = "\(systemVersion)"
-        }
-        if indexPath.row == 3 {
-            cell?.titleLabel.text = "모델"
-            cell?.descriptionLabel.text = "\(model)"
-        }
-        if indexPath.row == 4 {
-            cell?.titleLabel.text = "언어"
-            cell?.descriptionLabel.text = "\(localizedModel)"
-        }
-        if indexPath.row == 5 {
-            cell?.titleLabel.text = "UUID"
-//            cell?.descriptionLabel.text = "\(uuid)"
-        }
-        
-        
-        cell?.layout()
-        
-        return cell ?? UICollectionViewCell()
+    func navigationSetup() {
+        nameLabel.text = "iOS"
     }
 }
